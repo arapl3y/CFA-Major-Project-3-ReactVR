61aa418e1a68c1124b51f023444bec6daa061b18
Alex Rapley <rapley3@gmail.com>
2017-06-06 14:59:30 +1000

    Add api call functionality to import images

--- src/index.js
+++ src/index.js
@@ -3,32 +3,54 @@ import 'aframe-animation-component';
 import 'aframe-particle-system-component';
 import 'aframe-extras';
 import 'babel-polyfill';
-import {Entity, Scene} from 'aframe-react';
+import axios from 'axios';
+import { Entity, Scene } from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {color: 'red'};
+    this.state = {
+      imageIds: []
+    };
   }
 
-  // changeColor() {
-  //   const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-  //   this.setState({
-  //     color: colors[Math.floor(Math.random() * colors.length)]
-  //   });
-  // }
+  getImageIds() {
+    axios.get("http://localhost:3000/api/images/")
+      .then((images) => {
+        console.log(images.data);
+        this.setState({ imageIds: images.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  componentDidMount() {
+    this.getImageIds();
+  }
+
 
   render () {
+
+
     return (
-      <Scene>
+
+        <Scene>
         <a-assets>
 
           <a-asset-item id="gallery-obj" src="assets/cavanagh.obj"></a-asset-item>
           <a-asset-item id="gallery-mtl" src="assets/cavanagh.mtl"></a-asset-item>
 
           <img id="test" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg" />
+
+          {this.state.imageIds.map((image, i) =>
+            <img id={`test${i}`} src={`http://localhost:3000/images/${image._id}`} />
+          )}
+
+
+
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
           <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" />
         </a-assets>
@@ -38,13 +60,18 @@ class App extends React.Component {
         <Entity primitive="a-obj-model" src="#gallery-obj" mtl="#gallery-mtl" />
         <Entity primitive="a-box" position="0 1.5 -4" width="2.5" height="2" depth="0.1" src="#test" />
 
-        { /* Environment */ }
+        <Entity primitive="a-box" position="3 1.5 -2" width="2.5" height="2" depth="0.1" src="#test0" />
+        <Entity primitive="a-box" position="3 1.5 -1" width="2.5" height="2" depth="0.1" src="#test1" />
+        <Entity primitive="a-box" position="3 1.5 0" width="2.5" height="2" depth="0.1" src="#test2" />
+        <Entity primitive="a-box" position="3 1.5 1" width="2.5" height="2" depth="0.1" src="#test3" />
+        <Entity primitive="a-box" position="3 1.5 2" width="2.5" height="2" depth="0.1" src="#test4" />
+
+
         <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
         <Entity primitive="a-light" type="ambient" color="#333"/>
         <Entity primitive="a-light" type="point" color="#ECE1CD" angle="90" position="0 5 0"/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
 
-        { /* <Entity text={{value: 'Virtual Gallery', align: 'center'}} position={{x: 0, y: 2, z: -1}}/> */ }
 
         <Entity primitive="a-camera">
           <Entity primitive="a-cursor" />
