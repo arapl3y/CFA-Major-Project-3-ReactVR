75a6ba837d4a58e4d37edfd7eef341612dbe86a3
Alex Rapley <rapley3@gmail.com>
2017-06-06 13:20:09 +1000

    Add first successful test of rendering image in gallery

--- src/index.js
+++ src/index.js
@@ -24,8 +24,11 @@ class App extends React.Component {
     return (
       <Scene>
         <a-assets>
+
           <a-asset-item id="gallery-obj" src="assets/cavanagh.obj"></a-asset-item>
           <a-asset-item id="gallery-mtl" src="assets/cavanagh.mtl"></a-asset-item>
+
+          <img id="test" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg" />
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
           <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" />
         </a-assets>
@@ -33,7 +36,7 @@ class App extends React.Component {
 
 
         <Entity primitive="a-obj-model" src="#gallery-obj" mtl="#gallery-mtl" />
-        <Entity primitive="a-plane" />
+        <Entity primitive="a-box" position="0 1.5 -4" width="2.5" height="2" depth="0.1" src="#test" />
 
         { /* Environment */ }
         <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
