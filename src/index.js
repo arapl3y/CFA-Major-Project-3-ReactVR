4840b4417ea1d55e5f5ccd4683e122a5acb81762
Alex Rapley <rapley3@gmail.com>
2017-06-06 11:45:56 +1000

    Make light warmer for Jamie, add test plane entity

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {color: 'red'};
+    // this.state = {color: 'red'};
   }
 
-  changeColor() {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-    this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)]
-    });
-  }
+  // changeColor() {
+  //   const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+  //   this.setState({
+  //     color: colors[Math.floor(Math.random() * colors.length)]
+  //   });
+  // }
 
   render () {
     return (
@@ -30,15 +30,21 @@ class App extends React.Component {
           <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" />
         </a-assets>
 
-        <Entity primitive='a-obj-model' src="#gallery-obj" mtl="#gallery-mtl" />
+
+
+        <Entity primitive="a-obj-model" src="#gallery-obj" mtl="#gallery-mtl" />
+        <Entity primitive="a-plane" />
+
+        { /* Environment */ }
         <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
-        <Entity primitive="a-light" type="ambient" color="#445451"/>
-        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
+        <Entity primitive="a-light" type="ambient" color="#333"/>
+        <Entity primitive="a-light" type="point" color="#ECE1CD" angle="90" position="0 5 0"/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity text={{value: 'Virtual Gallery', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+
+        { /* <Entity text={{value: 'Virtual Gallery', align: 'center'}} position={{x: 0, y: 2, z: -1}}/> */ }
 
         <Entity primitive="a-camera">
-          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+          <Entity primitive="a-cursor" />
         </Entity>
       </Scene>
     );
