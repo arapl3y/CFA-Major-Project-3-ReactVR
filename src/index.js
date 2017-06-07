34e2610c4a2e14c93f439b91834d7927f69d0b32
Alex Rapley <rapley3@gmail.com>
2017-06-07 16:16:46 +1000

    Utilise new routes set by Node App to bring in dynamic content from different gallery query params

--- src/index.js
+++ src/index.js
@@ -16,66 +16,65 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.getImageIds()
+  }
+
   getImageIds() {
-    axios.get("http://localhost:3000/api/images/")
+    axios.get(`http://localhost:3000/api/images/${window.location.search}`)
       .then((images) => {
-        console.log(images.data);
         this.setState({ imageIds: images.data });
+        console.log(this.state.imageIds)
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
-  componentDidMount() {
-    this.getImageIds();
-  }
-
 
   render () {
-
-
+    console.log(this.state.imageIds);
     return (
 
         <Scene>
-        <a-assets>
+          <a-assets>
 
-          <a-asset-item id="gallery-obj" src="assets/cavanagh.obj"></a-asset-item>
-          <a-asset-item id="gallery-mtl" src="assets/cavanagh.mtl"></a-asset-item>
+            <a-asset-item id="gallery-obj" src="assets/cavanagh.obj"></a-asset-item>
+            <a-asset-item id="gallery-mtl" src="assets/cavanagh.mtl"></a-asset-item>
 
-          <img id="test" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg" />
+            <img id="van-gogh" alt="starry" src="//upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg" />
 
-          {this.state.imageIds.map((image, i) =>
-            <img id={`test${i}`} src={`http://localhost:3000/images/${image._id}`} />
-          )}
+            {this.state.imageIds.map((image, i) =>
+              <img id={`test${i}`} alt="painting" key={i} src={`http://localhost:3000/images/${image._id}`} />
+            )}
 
-
-
-          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
-          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" />
-        </a-assets>
+            <img id="groundTexture" alt="ground" src="//cdn.aframe.io/a-painter/images/floor.jpg" />
+            <img id="skyTexture" alt="sky" src="//cdn.aframe.io/a-painter/images/sky.jpg" />
+          </a-assets>
 
 
 
-        <Entity primitive="a-obj-model" src="#gallery-obj" mtl="#gallery-mtl" />
-        <Entity primitive="a-box" position="0 1.5 -4" width="2.5" height="2" depth="0.1" src="#test" />
-
-        <Entity primitive="a-box" position="3 1.5 -2" width="2.5" height="2" depth="0.1" src="#test0" />
-        <Entity primitive="a-box" position="3 1.5 -1" width="2.5" height="2" depth="0.1" src="#test1" />
-        <Entity primitive="a-box" position="3 1.5 0" width="2.5" height="2" depth="0.1" src="#test2" />
-        <Entity primitive="a-box" position="3 1.5 1" width="2.5" height="2" depth="0.1" src="#test3" />
-        <Entity primitive="a-box" position="3 1.5 2" width="2.5" height="2" depth="0.1" src="#test4" />
+          <Entity primitive="a-obj-model" src="#gallery-obj" mtl="#gallery-mtl" />
 
 
-        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
-        <Entity primitive="a-light" type="ambient" color="#333"/>
-        <Entity primitive="a-light" type="point" color="#ECE1CD" angle="90" position="0 5 0"/>
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
+
+          <Entity primitive="a-box" position="0 1.5 -4" width="2.5" height="2" depth="0.1" src="#test0" />
+          <Entity primitive="a-box" position="-4 1.5 0" rotation="0 90" width="2.5" height="2" depth="0.1" src="#test1" />
+          <Entity primitive="a-box" position="4 1.5 0" rotation="0 90" width="2.5" height="2" depth="0.1" src="#test2" />
+          <Entity primitive="a-box" position="0 1.5 4" width="2.5" height="2" depth="0.09" src="#test3" />
 
 
-        <Entity primitive="a-camera">
-          <Entity primitive="a-cursor" />
-        </Entity>
+          { /* <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/> */ }
+
+          <Entity primitive="a-light" type="ambient" color="#333"/>
+          <Entity primitive="a-light" type="point" color="#ECE1CD" angle="90" position="0 5 0"/>
+
+          { /* <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/> */}
+
+
+          <Entity primitive="a-camera">
+            <Entity primitive="a-cursor" />
+          </Entity>
       </Scene>
     );
   }
